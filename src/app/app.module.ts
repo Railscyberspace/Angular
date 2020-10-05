6e88510a1bffbb6eb5295b581dffe65929d554f6
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2020-10-05 19:40:15 +0100

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,63 @@
-import { Http, Response } from '@angular/http';
+// tslint:disable-next-line:quotemark
+import { Http, Response } from "@angular/http";
 
-import { Injectable } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// tslint:disable-next-line:quotemark
+import { Injectable } from "@angular/core";
+// tslint:disable-next-line:quotemark
+import { BrowserModule } from "@angular/platform-browser";
+// tslint:disable-next-line:quotemark
+import { NgModule } from "@angular/core";
+// tslint:disable-next-line:quotemark
+import { FormsModule } from "@angular/forms";
+// tslint:disable-next-line:quotemark
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { constructor } from 'assert';
-import { getLocaleNumberSymbol } from '@angular/common';
+// tslint:disable-next-line:quotemark
+import { AppComponent } from "./app.component";
+// tslint:disable-next-line:quotemark
+import { ProductPageComponent } from "./product-page/product-page.component";
+// tslint:disable-next-line:quotemark
+import { ProductDescriptionComponent } from "./product-description/product-description.component";
+// tslint:disable-next-line:quotemark
+import { constructor } from "assert";
+// tslint:disable-next-line:quotemark
+import { getLocaleNumberSymbol } from "@angular/common";
 
-
-import 'rxjs/add/operator/map';
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+// tslint:disable-next-line:quotemark
+import "rxjs/add/operator/map";
+// tslint:disable-next-line:quotemark
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+// tslint:disable-next-line:quotemark
+import { ProductListComponent } from "./product-list/product-list.component";
+// tslint:disable-next-line:quotemark
+import { RouterModule, Router } from "@angular/router";
+// tslint:disable-next-line:quotemark
 @Injectable()
-export class ProductService
-{
+export class ProductService {
+  // tslint:disable-next-line:quotemark
+  private _albumUrl = "...assets/album.json";
+  constructor(private _http: Http) {}
 
-
-  private const _albumUrl = '...assets/album.json';
-  constructor(private _http: Http) { }
-
-  getAlbum(id: number)
-  {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(
       // tslint:disable-next-line:no-shadowed-variable
-      (Response => Response.json()));
+      (Response) => Response.json()
+    );
   }
 }
-
+appRoutes({
+  Routes: [path, products, ProductListComponent],
+});
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
-    ProductListComponent
+    ProductListComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
